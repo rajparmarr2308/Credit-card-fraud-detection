549d35eeee2a51407fb4bd6d370c8ce1a0b49b95
Raj Parmar <rajparmar23801@gmail.com>
2021-03-26 23:37:03 +0530

    added bluprint of project structure

--- Apps/homeApp/urls.py
+++ Apps/homeApp/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import base
+from .views import base,login,about
 
 urlpatterns = [
     path('',base),
+    path('login/',login,name='login'),
+    path('about/',about,name='about'),
 ]
--- Apps/homeApp/views.py
+++ Apps/homeApp/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http.response import HttpResponseRedirect,HttpResponse
 
 def base(request):
-    return render(request,'homeApp/base.html')
+    return render(request,'homeApp/landing_page.html')
+
+
+def login(request):
+    return render(request,'homeApp/login.html')
+
+def about(request):
+    return render(request,'homeApp/about.html')
